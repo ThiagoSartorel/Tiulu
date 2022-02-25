2b861af590653491688b11e7283bad1e26af55ec
Thiago Sartorel <54656829+ThiagoSartorel@users.noreply.github.com>
2022-02-25 13:42:36 -0300

    Update Form1.Designer.cs

--- Projeto1/Projeto1/Form1.Designer.cs
+++ Projeto1/Projeto1/Form1.Designer.cs
@@ -9,6 +9,7 @@
 
         /// <summary>
         ///  Clean up any resources being used.
+        /// Thiago Sartorel
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
